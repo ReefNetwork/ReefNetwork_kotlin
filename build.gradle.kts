daf71c3a3c897ec2d92ee6bc416351018faa5c62
Ree_ <ree.dev.jp@gmail.com>
2020-02-16 23:18:38 +0900

    簡単にコンパイル出来るように

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.3.60"
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
     id("net.minecrell.plugin-yml.nukkit") version "0.3.0"
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
-group = "com.ree"
+group = "com.ree.reefnetwork"
 version = "2.0-SNAPSHOT"
 
 repositories {
@@ -15,9 +15,9 @@ repositories {
 }
 
 dependencies {
-    shadow(kotlin("stdlib"))
     compileOnly("cn.nukkit:nukkit:2.0.0-SNAPSHOT")
     testCompileOnly("cn.nukkit:nukkit:2.0.0-SNAPSHOT")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks {
@@ -32,7 +32,7 @@ tasks {
 
 nukkit {
     name = "ReefNetWork"
-    main = "com.ree.reefnetwork.Core"
+    main = "com.ree.reefnetwork.ReefNetworkPlugin"
     api = listOf("2.0.0")
     author = "Ree-jp"
     description = "ReefNetWorkPlugin"
---
+++ run.bat
@@ -0,0 +1,9 @@
+start minecraft.bat
+move build\libs\ReefNetwork-2.0-SNAPSHOT-all.jar D:\nukkit2.0\plugins
+cd D:\nukkit2.0
+@color 0a
+@echo StartingServer...
+java -jar Nukkit.jar
+@color 00
+@echo Initializing...
+@cd D:\code\nukkitPlugin\ReefNetwork

7585699962a46ab9fe9e3daec14cc06de08ddce0
Ree_ <ree.dev.jp@gmail.com>
2020-02-20 23:28:43 +0900

    add sqlite connect, close

--- src/main/kotlin/com/ree/reefnetwork/sub/login/db/LoginDb.kt
+++ src/main/kotlin/com/ree/reefnetwork/sub/login/db/LoginDb.kt
@@ -1,8 +1,41 @@
 package com.ree.reefnetwork.sub.login.db
 
-class LoginDb (pass: String) {
+import java.lang.ClassNotFoundException
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.sql.Statement
+
+class LoginDb(private val pass: String) {
+
+    private lateinit var connection: Connection
+    private lateinit var statement: Statement
 
     init {
+        connect()
+    }
 
+    private fun connect () {
+        try {
+            Class.forName("org.sqlite.JDBC")
+            connection = DriverManager.getConnection("jdbc:sqlite.${pass}")
+            statement = connection.createStatement()
+
+        } catch (ex: ClassNotFoundException) {
+            ex.printStackTrace()
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+        }
+    }
+
+    private fun close() {
+        try {
+            connection.close()
+            statement.close()
+        } catch (ex: NullPointerException) {
+            ex.printStackTrace()
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+        }
     }
 }
